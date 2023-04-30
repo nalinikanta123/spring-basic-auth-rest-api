3104f99769811ab9f7f73e5c029042a1d6333283
Nalini Kanta Mallick <aaradhyashrinayjhili@Nalinis-MacBook-Air.local>
2023-04-30 14:14:54 -0400

    basic authentication demo application

---
+++ src/main/java/com/dream/thoughts/springbasicauthrestapi/SpringBasicAuthRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dream.thoughts.springbasicauthrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAuthRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicAuthRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/thoughts/springbasicauthrestapi/SpringBasicAuthRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.thoughts.springbasicauthrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAuthRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
